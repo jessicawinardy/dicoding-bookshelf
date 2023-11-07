b8c0d1abbefea8af5a22f4b6e598bf864a948ff2
jessicawinardy <jessicawinardy@gmail.com>
2023-11-07 14:30:42 +0800

    .

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-const buku = [];
+const books = [];
 const RENDER_BOOKS = 'render-books';
 const SAVE_BOOKS = 'save-books';
 const STORAGE_KEY = 'BOOKSHELF-APP'
@@ -18,5 +18,46 @@ document.addEventListener('DOMContentLoaded', function(){
         addBook();
     });
 
-    
+    function loadDataFromStorage(){
+        const serializedData = localStorage.getItem(STORAGE_KEY);
+        let data = JSON.parse(serializedData);
+
+        if (data !== null){
+            for (const book of data) {
+                books.push(book);
+                
+            }
+        }
+
+        document.dispatchEvent(new Event(RENDER_BOOKS));
+    }
+
+    function addBook(){
+        const judulBuku = document.getElementById('input-judul').value;
+        const penulisBuku = document.getElementById('input-penulis').value;
+        const tahunBuku = document.getElementById('input-tahun').value;
+        const isRead = document.getElementById('input-selesai').checked;
+
+        const generateID = generateId();
+        const bookObject = generateBookObject(generateID, judulBuku, penulisBuku, tahunBuku, isRead);
+        books.push(bookObject);
+
+        document.dispatchEvent(new Event(RENDER_BOOKS));
+        updateDataToStorage();
+    }
+
+    function generateId(){
+        return +new Date();
+    }
+
+    function generateBookObject(id, judul, penulis, tahun){
+        return {
+            id,
+            judul,
+            penulis,
+            tahun
+        };
+    }
+
+
 });
